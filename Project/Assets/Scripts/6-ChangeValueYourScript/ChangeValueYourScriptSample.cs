649b99bb27de507a4d802a9320f5862a2edcbc8a
inolalala <inoue-sota@kayac.com>
2020-07-11 10:28:56 +0900

    add DOTween.To() sample

---
+++ Project/Assets/Scripts/6-ChangeValueYourScript/ChangeValueYourScriptSample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChangeValueYourScriptSample : MonoBehaviour
+{
+    /*
+    DOTweenにはDOMoveやDOColorみたいにTransformやMaterialを拡張したメソッドがたくさんあって、
+    基本それを使えばいいのだけど(TrailRendererのパラメータを弄るのに便利なやつ、みたいなニッチな物すら用意されている)、
+    例えば自作スクリプトの値をtweenさせたい！みたいな事があった時は
+    DOTween.To()で実装してあげると良い。
+    */
+     [Header("Tween結果を見るだけなので触らなくてOK")]
+    [SerializeField] private float targetValue;
+    void Start()
+    {
+        DOTween.To(
+            () => targetValue,
+            (v) => targetValue = v,
+            100f,
+            5f
+        ).SetLoops(-1,LoopType.Yoyo);
+    }
+}
