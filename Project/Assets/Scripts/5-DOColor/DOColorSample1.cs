c76d92b2610d8ae360997476d94b718e203c1c5c
inolalala <inoue-sota@kayac.com>
2020-07-11 10:16:29 +0900

    add DOColor

---
+++ Project/Assets/Scripts/5-DOColor/DOColorSample1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DOColorSample1 : MonoBehaviour
+{
+    [SerializeField] private Renderer targetRender;
+    void Start()
+    {
+        //DOTweenはコールバックを指定できる。OnCompleteで「終了時に何をするか」を書ける。
+        transform.DOMoveX(5f,3f).OnComplete(
+            () => {
+                Debug.Log("OnComplete!");
+                targetRender.material.DOColor(Color.red,3f);//3秒かけて赤くなる
+            }
+        );
+    }
+}
