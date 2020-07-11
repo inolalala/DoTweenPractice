1d27f0cb7145650893c3691da1f80a82eea51c9e
inolalala <inoue-sota@kayac.com>
2020-07-11 09:33:24 +0900

    fix DOMove add DORotate

--- Project/Assets/Scripts/1-DOMove/DOMoveSample1.cs
+++ Project/Assets/Scripts/1-DOMove/DOMoveSample1.cs
@@ -8,11 +8,11 @@ public class DOMoveSample1 : MonoBehaviour
     void Start()
     {
         transform.DOMove(
-            new Vector3(5f,0f,0f),//目的地
+            new Vector3(5f,0f,0f),//目的のパラメータ
             5f//かける秒数
         )
-        .SetLoops(-1)//繰り返し(引数に繰り返す回数をいれる。-1で無限ループ)
-        .Play();//再生
+        .SetLoops(-1);//繰り返し(引数に繰り返す回数をいれる。-1で無限ループ)
+        
     }
 
 }
---
+++ Project/Assets/Scripts/2-DORotate/DORotateSample1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DORotateSample1 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DORotate(
+            new Vector3(60f,60f,60f),//目的のパラメータ
+            3f//かける秒数
+        )
+        .SetLoops(-1,LoopType.Yoyo);//ヨーヨーのような行ったり来たりの繰り返し
+    }
+}
