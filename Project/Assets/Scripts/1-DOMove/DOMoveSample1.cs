32d203df09de30f946bce0ed4f50d9bd49909ca7
inolalala <inoue-sota@kayac.com>
2020-07-11 09:20:12 +0900

    add DOMoveSample

---
+++ Project/Assets/Scripts/1-DOMove/DOMoveSample1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DOMoveSample1 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMove(
+            new Vector3(5f,0f,0f),//目的地
+            5f//かける秒数
+        )
+        .SetLoops(-1)//繰り返し(引数に繰り返す回数をいれる。-1で無限ループ)
+        .Play();//再生
+    }
+
+}
